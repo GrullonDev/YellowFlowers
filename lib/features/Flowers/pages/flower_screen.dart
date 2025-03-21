f6e4867dccef84136d4ab972c0193d1a70b761a9
GrullonDev <prosystem155@gmail.com>
2025-03-21 00:52:44 -0600

    feat: mejora en la pantalla de flores amarillas para que se pueda visualizar mas dinamica

--- lib/features/Flowers/pages/flower_screen.dart
+++ lib/features/Flowers/pages/flower_screen.dart
@@ -1,115 +1,244 @@
-import 'package:flutter/material.dart';
-import 'dart:math' as math;
 import 'dart:async';
+import 'dart:math' as math;
+
+import 'package:flutter/material.dart';
 
 import 'package:yellow_flowers/features/Flowers/widgets/flower.dart';
 
 class FlowerScreen extends StatefulWidget {
-  const FlowerScreen({super.key});
+  final String recipientName;
+  const FlowerScreen({super.key, required this.recipientName});
 
   @override
   State<FlowerScreen> createState() => _FlowerScreenState();
 }
 
 class _FlowerScreenState extends State<FlowerScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-  late Animation<double> _animation;
-
-  List<Offset> _flowerPositions = [];
-  final int _flowerCount = 85;
+    with TickerProviderStateMixin {
+  late List<AnimationController> _flowerControllers = [];
+  late AnimationController _messageAnimationController;
   late Timer _timer;
+  late List<AnimationController> _sparkleControllers = [];
+  final int _sparkleCount = 50;
+
+  final int _flowerCount = 60;
+  final List<String> _messages = [
+    "Gracias por ser parte de mi vida, contigo todo es mejor. ❤️❤️",
+    "Eres la flor más hermosa de mi jardín. 🌼",
+    "Tu sonrisa ilumina mis días. ☀️ ☀️ ☀️",
+    "Contigo, cada momento es especial. ✨",
+    "Eres mi compañera, amiga y amor. 💖",
+  ];
 
   @override
   void initState() {
     super.initState();
 
-    _generateRandomFlowerPositions();
-
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(seconds: 3),
-    )..repeat(reverse: true);
-
-    _animation = Tween<double>(
-      begin: 500,
-      end: 100,
-    ).animate(
-      CurvedAnimation(
-        parent: _controller,
-        curve: Curves.easeInOut,
-      ),
+    _flowerControllers = List.generate(
+      _flowerCount,
+      (index) => AnimationController(
+        vsync: this,
+        duration: const Duration(microseconds: 5),
+      )..repeat(reverse: true),
     );
 
-    _timer = Timer.periodic(const Duration(seconds: 10), (timer) {
-      setState(() {
-        _generateRandomFlowerPositions();
-      });
+    _sparkleControllers = List.generate(
+      _sparkleCount,
+      (index) => AnimationController(
+        vsync: this,
+        duration: Duration(
+          milliseconds: 1500 + math.Random().nextInt(1500),
+        ),
+      )..repeat(reverse: true),
+    );
+
+    _messageAnimationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 2),
+    )..forward();
+
+    _timer = Timer.periodic(const Duration(seconds: 5), (timer) {
+      _resetFlowerPositions();
+      _showRandomMessage();
+      _resetSparkleAnimations();
+      setState(() {});
     });
   }
 
-  void _generateRandomFlowerPositions() {
-    _flowerPositions = List.generate(
-      _flowerCount,
-      (index) => Offset(
-        math.Random().nextDouble() * 650,
-        math.Random().nextDouble() * 950,
-      ),
-    );
+  void _resetFlowerPositions() {
+    for (int i = 0; i < _flowerCount; i++) {
+      _flowerControllers[i].reset();
+      _flowerControllers[i].forward();
+    }
+  }
+
+  void _resetSparkleAnimations() {
+    for (int i = 0; i < _sparkleCount; i++) {
+      _sparkleControllers[i].reset();
+      _sparkleControllers[i].forward();
+    }
+  }
+
+  void _showRandomMessage() {
+    setState(() {
+      // No necesitamos lógica adicional aquí
+    });
+  }
+
+  Color _getBackgroundColor() {
+    final now = DateTime.now();
+    final hour = now.hour;
+
+    if (hour >= 6 && hour < 12) {
+      return const Color.fromRGBO(255, 248, 220, 1);
+    } else if (hour >= 12 && hour < 18) {
+      return const Color.fromRGBO(255, 228, 196, 1);
+    } else if (hour >= 18 && hour < 22) {
+      return const Color.fromRGBO(255, 182, 193, 1);
+    } else {
+      return const Color.fromRGBO(173, 216, 230, 1);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    // Calcular la altura del mensaje de manera dinámica
+    final messageHeight =
+        screenHeight * 0.10; // 10% de la altura de la pantalla
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Flores Amarillas'),
       ),
-      body: Container(
-        color: Colors.lightBlue[100], // Cielo
-        child: Stack(
-          children: [
-            Container(
-              width: double.infinity,
-              height: double.infinity,
-              color: Colors.green[200], // Césped
-            ),
-            ...List.generate(
-              _flowerCount,
-              (index) => Positioned(
-                left: _flowerPositions[index].dx,
-                top: _flowerPositions[index].dy,
-                child: const Flor(),
+      body: Stack(
+        children: [
+          Container(
+            color: _getBackgroundColor(),
+          ),
+
+          // Mensaje
+          Positioned(
+            top: screenHeight * 0.05, // Ajuste para posicionar el mensaje
+            left: screenWidth * 0.1,
+            width: screenWidth * 0.8,
+            child: Opacity(
+              opacity: _messageAnimationController.value,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  "${widget.recipientName}, ${_messages[math.Random().nextInt(_messages.length)]}",
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
               ),
             ),
-            AnimatedBuilder(
-              animation: _animation,
-              builder: (_, __) {
-                return Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    color: Colors.transparent,
-                    child: const Text(
-                      'Gracias por ser parte de mi vida, \ncontigo todo es mejor. ❤️❤️',
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 50,
-                        fontWeight: FontWeight.bold,
+          ),
+
+          // Destellos
+          ...List.generate(
+            _sparkleCount,
+            (index) => AnimatedBuilder(
+              animation: _sparkleControllers[index],
+              builder: (context, child) {
+                final x = math.Random().nextDouble() * screenWidth;
+                final y = messageHeight +
+                    math.Random().nextDouble() *
+                        (screenHeight * 0.4); // Acorta el rango de destellos
+                final size = 3.0 + _sparkleControllers[index].value * 5.0;
+                final opacity = 0.5 + _sparkleControllers[index].value * 0.5;
+
+                return Positioned(
+                  left: x,
+                  top: y,
+                  child: Opacity(
+                    opacity: opacity,
+                    child: Container(
+                      width: size,
+                      height: size,
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.yellow.withOpacity(0.3),
+                            blurRadius: 5,
+                            spreadRadius: 2,
+                          ),
+                        ],
                       ),
                     ),
                   ),
                 );
               },
             ),
-          ],
-        ),
+          ),
+
+          // Flores
+          ...List.generate(
+            _flowerCount,
+            (index) => AnimatedBuilder(
+              animation: _flowerControllers[index],
+              builder: (context, child) {
+                final x = math.Random().nextDouble() * screenWidth;
+                final y = screenHeight * 0.25 +
+                    math.Random().nextDouble() *
+                        (screenHeight *
+                            0.4); // Ajuste la posición Y para que no se superponga
+                final scale = 1.4 +
+                    math.sin(_flowerControllers[index].value * 2 * math.pi) *
+                        0.10;
+                return Positioned(
+                  left: x,
+                  top: y,
+                  child: Transform.scale(
+                    scale: scale,
+                    child: SizedBox(
+                      // Envuelve Flor con SizedBox
+                      width: screenWidth / 8, // Ancho de la flor
+                      height: screenHeight /
+                          2, // Altura suficiente para mostrar toda la flor
+                      child: const Flor(),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
 
   @override
   void dispose() {
-    _controller.dispose();
+    for (var controller in _flowerControllers) {
+      controller.dispose();
+    }
+    for (var controller in _sparkleControllers) {
+      controller.dispose();
+    }
+    _messageAnimationController.dispose();
     _timer.cancel();
     super.dispose();
   }
 }
+
+/* final int _flowerCount = 60; // Reducimos un poco para mejor rendimiento
+final List<String> _messages = [
+  "Gracias por ser parte de mi vida, contigo todo es mejor. ❤️❤️",
+  "Eres la flor más hermosa de mi jardín. 🌼",
+  "Tu sonrisa ilumina mis días. ☀️ ☀️ ☀️",
+  "Contigo, cada momento es especial. ✨",
+  "Eres mi compañera, amiga y amor. 💖",
+]; */
--- lib/features/home/pages/home_layout.dart
+++ lib/features/home/pages/home_layout.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:yellow_flowers/features/Flowers/pages/flower_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:yellow_flowers/features/Flowers/bloc/flower_bloc.dart';
+
+import 'package:yellow_flowers/features/Flowers/widgets/name_entry_flower.dart';
 import 'package:yellow_flowers/utils/buttons/elevated_button.dart';
 
 class HomeLayout extends StatelessWidget {
@@ -7,43 +10,46 @@ class HomeLayout extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Menu'),
+    return ChangeNotifierProvider(
+      create: (context) => FlowerBloc(),
+      builder: (context, __) => Scaffold(
+        appBar: AppBar(
+          title: const Center(
+            child: Text('Menu'),
+          ),
         ),
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          AppElevatedButton(
-            onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const Placeholder(),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            AppElevatedButton(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const Placeholder(),
+                ),
               ),
+              title: 'lalalalalala',
             ),
-            title: 'lalalalalala',
-          ),
-          const SizedBox(height: 20),
-          const Text('Hola Mundo!!'),
-          const SizedBox(height: 20),
-          const Text('Hola Mundo!!'),
-          const SizedBox(height: 20),
-          const Text('Hola Mundo!!'),
-          const SizedBox(height: 20),
-          const Text('Hola Mundo!!'),
-          const SizedBox(height: 20),
-          AppElevatedButton(
-            onTap: () => Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => const FlowerScreen(),
+            const SizedBox(height: 20),
+            const Text('Hola Mundo!!'),
+            const SizedBox(height: 20),
+            const Text('Hola Mundo!!'),
+            const SizedBox(height: 20),
+            const Text('Hola Mundo!!'),
+            const SizedBox(height: 20),
+            const Text('Hola Mundo!!'),
+            const SizedBox(height: 20),
+            AppElevatedButton(
+              onTap: () => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const NameEntryFlower(),
+                ),
               ),
+              title: 'Flores Amarillas',
             ),
-            title: 'Flores Amarillas',
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
