efed57502c4abbf54b0639a2b433077733c3605e
GrullonDev <prosystem155@gmail.com>
2025-03-21 10:00:25 -0600

    feat: mejora en el nombre de las importaciones y actualización del menú en HomeBloc

--- lib/features/home/bloc/home_bloc.dart
+++ lib/features/home/bloc/home_bloc.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:yellow_flowers/features/flowers/widgets/name_entry_flower.dart';
 import 'package:yellow_flowers/features/home/data/object/menu_item.dart';
+import 'package:yellow_flowers/features/romantic_music/pages/romantic_music_page.dart';
 import 'package:yellow_flowers/utils/base_model.dart';
 
 class HomeBloc extends BaseModel {
@@ -24,7 +25,7 @@ class HomeBloc extends BaseModel {
         MenuItem(
           title: 'Música Romántica',
           icon: Icons.music_note,
-          destination: const Scaffold(),
+          destination: const RomanticMusicPage(),
         ),
         MenuItem(
           title: 'Tarjetas Virtuales',
