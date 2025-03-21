2856fac90a213ddec7769d6e00446c103f1d28dc
GrullonDev <prosystem155@gmail.com>
2025-03-21 13:08:06 -0600

    feat: modificar el nombre de la ruta de romantic_music a music

--- lib/features/romantic_music/pages/music_page.dart
+++ lib/features/romantic_music/pages/music_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:yellow_flowers/features/romantic_music/bloc/romantic_music_bloc.dart';
 import 'package:yellow_flowers/features/romantic_music/data/repository/music_remote_repository.dart';
-import 'package:yellow_flowers/features/romantic_music/pages/romantic_music_layout.dart';
+import 'package:yellow_flowers/features/romantic_music/pages/music_layout.dart';
 import 'package:yellow_flowers/utils/base_model_scaffold.dart';
 import 'package:yellow_flowers/utils/inyenction_container.dart' as sl;
 
